e36e4db8d72bc46f22af9dc256efbbbfd274977f
Gurwinder Singh <55468325+gurwinder13@users.noreply.github.com>
2020-01-09 00:25:01 -0500

    Update board_api.h

--- lpc_board_nxp_lpcxpresso_1769/inc/board_api.h
+++ lpc_board_nxp_lpcxpresso_1769/inc/board_api.h
@@ -6,7 +6,7 @@
  * All rights reserved.
  *
  * @par
- * Software that is described herein is for illustrative purposes only
+ * Software that is described    herein is for illustrative purposes only
  * which provides customers with programming information regarding the
  * LPC products.  This software is supplied "AS IS" without any warranties of
  * any kind, and NXP Semiconductors and its licensor disclaim any and
